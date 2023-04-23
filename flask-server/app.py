b0bd55f781d3078e6ab66a90a142bcccaf4eaf25
hans@dizzi.biz <hans@dizzi.biz>
2023-04-24 01:08:04 +0200

    first session fun

---
+++ flask-server/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect, url_for, make_response
+from random import randint
+
+import os
+
+app = Flask(__name__)
+
+# Startseite
+@app.route('/')
+def welcome():
+    # generate random numeric value between 0 and 1000000
+    # and use it as a session id
+    id = randint(0, 1000000)
+    resp = make_response(render_template('welcome.html'))
+    resp.set_cookie('id', str(id))
+    return resp
+
+@app.route('/welcomeConfirm')
+def welcomeConfirm():
+    return redirect(url_for('really'))
+
+@app.route('/really')
+def really():
+    return render_template('really.html', id=request.cookies.get('id'))
+
+@app.route('/reallyConfirm')
+def reallyConfirm():
+    return redirect(url_for('dsgvo'))
+
+@app.route('/dsgvo')
+def dsgvo():
+    return render_template('DSGVO.html', id=request.cookies.get('id'))

e5a9226c0bb99fe1c7903891ac9dea002b997e6f
hans@dizzi.biz <hans@dizzi.biz>
2023-04-24 02:15:49 +0200

    added pseudocode for qr-mechanism

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, make_response
 from random import randint
+from time import sleep
+
 # Hallo Hans
 import os
+
+qrLoadedFlag = False
 
 app = Flask(__name__)
 
@@ -30,3 +34,29 @@ def reallyConfirm():
 @app.route('/dsgvo')
 def dsgvo():
     return render_template('DSGVO.html', id=request.cookies.get('id'))
+
+@app.route('/dsgvoConfirm')
+def dsgvoConfirm():
+    return render_template('qrCode.html')
+
+@app.route('/qrCode')
+def qrCode():
+    # generate session for user and persist to database
+    # reset qrLoadedFlag
+    return render_template('qrCode.html')
+
+@app.route('/qrLoaded')
+def qrLoaded():
+    while (not qrLoadedFlag):
+        # read qrLoadedFlag from database (will be set when fon open fonWelcome)
+        print ("waiting for qr code scan")
+        # delay for 1 second
+        sleep(1)
+        pass
+    return render_template('qrLoaded.html')
+
+# fon
+@app.route('/fonWelcome')
+def fonWelcome():
+    # set qrLoadedFlag in database to true
+    return render_template('fonWelcome.html')

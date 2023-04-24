2c5141c384785a74cf94a21b67c7e0369dcc51e1
alphi1977 <bautista@gmx.de>
2023-04-24 03:58:00 +0200

    add datbase

--- flask-server/app.py
+++ flask-server/app.py
@@ -5,14 +5,13 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+db = SQLAlchemy()
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'teledisko.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
 
 class User(db.Model):
     __tablename__ = 'teledisko'
@@ -22,15 +21,10 @@ class User(db.Model):
     createdAt = db.Column(db.DateTime, default=datetime.utcnow)
     qrLoadedFlag = db.Column(db.Boolean, default=False)
 
+db.init_app(app)
 
-
-app = Flask(__name__)
-
-# Startseite
 @app.route('/')
 def welcome():
-    # generate random numeric value between 0 and 1000000
-    # and use it as a session id
     id = randint(0, 1000000)
     resp = make_response(render_template('welcome.html'))
     resp.set_cookie('id', str(id))
@@ -58,30 +52,23 @@ def dsgvoConfirm():
 
 @app.route('/qrCode')
 def qrCode():
-    # generate session for user and persist to database
-    # reset qrLoadedFlag
     return render_template('qrCode.html')
 
 @app.route('/qrLoaded')
 def qrLoaded():
     while (not True):
-        # read qrLoadedFlag from database (will be set when fon open fonWelcome)
         print ("waiting for qr code scan")
-        # delay for 1 second
         sleep(1)
         pass
     return render_template('qrLoaded.html')
 
-# fon
 @app.route('/fonWelcome')
 def fonWelcome():
-    # set qrLoadedFlag in database to true
     return render_template('fonWelcome.html')
 
 if(__name__ == '__main__'):
-   
-    with app.app_context():
-       print('Creating database tables...')
-       db.create_all()
-       print('Done.')
+    # with app.app_context():
+    #     print('Creating database tables...')
+    #     db.create_all()
+    #     print('Done.')
     app.run(debug=True)

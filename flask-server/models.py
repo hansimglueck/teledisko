6c6ba2f0973a6b5887c6416a032a374964bbe6d5
8JupiterMoll8 <bautista@gmx.de>
2023-05-09 15:54:36 +0100

    addconfig.py

---
+++ flask-server/models.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'teledisko'
+    id = db.Column(db.Integer, primary_key=True)
+    videoFile = db.Column(db.String(120), unique=False)
+    sessionId = db.Column(db.String(120), unique=False, nullable=False)
+    createdAt = db.Column(db.DateTime, default=datetime.utcnow)
+    qrLoadedFlag = db.Column(db.Boolean, default=False)
+    videoReayToDownloadFlag = db.Column(db.Boolean, default=False)

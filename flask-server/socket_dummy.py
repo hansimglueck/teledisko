013e5fc3dbd49d817a61d1aaa1263299e00b6d77
hansimglueck <hans@dizzi.biz>
2023-05-13 00:26:15 +0200

    tidy up

--- flask-server/socket_dummy.py
+++ flask-server/socket_dummy.py
@@ -14,45 +14,20 @@ app = Flask(__name__)
 
 choice = 'x'
 
-@app.route('/a')
-def a():
-    # Hier kommt der Code hin, der passiert, wenn der player fertig ist
-    global choice
-    choice = 'a'
-    return 'A'
-
-@app.route('/b')
-def b():
-    # Hier kommt der Code hin, der passiert, wenn der player fertig ist
-    global choice
-    print(choice)
-    choice = 'b'
-    return 'B'
-
-@app.route('/test')
-def test():
-    # Hier kommt der Code hin, der passiert, wenn der player fertig ist
-    global choice
-    print(choice)
-    return choice
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/play')
 def play():
-    # startRecord()
-    # socket.sendPlay()
     return render_template('play.html')
 
 @app.route('/complete')
 def complete():
-    # Hier kommt der Code hin, der den player startet
+
     # startRecord()
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        
         s.connect((host, port))
         s.sendall(b'play')
         response = s.recv(1024).decode('utf-8')
@@ -62,6 +37,34 @@ def complete():
     # stopRecord()
 
     return 'player completed!'
+
+
+
+
+
+
+@app.route('/a')
+def a():
+    global choice
+    choice = 'a'
+    return 'A'
+
+@app.route('/b')
+def b():
+    global choice
+    print(choice)
+    choice = 'b'
+    return 'B'
+
+@app.route('/test')
+def test():
+    global choice
+    print(choice)
+    return choice
+
+
+
+
 
 
 # Server starten

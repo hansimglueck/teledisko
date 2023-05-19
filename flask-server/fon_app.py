2e894076a9b9391f2b4e6fc1043fd81d62c75b9e
8JupiterMoll8 <bautista@gmx.de>
2023-05-19 04:10:09 +0100

    add commets to fon_app

--- flask-server/fon_app.py
+++ flask-server/fon_app.py
@@ -10,56 +10,85 @@ app.config.from_object(Config)
 db.init_app(app)
 
 
-
+############################################################################
+##################Insert  und validate  Password for Download ##############
+############################################################################
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         session_id = request.form['session_id']
+
         user = User.query.filter_by(sessionId=session_id).first()
+
         if user:
-            if user.downloaded:
+
+                # Video wurde mit diesem Password schonmal runtergeladen
+            if user.downloaded: 
+
                 return render_template('fon_video_downloaded.html')
+            
+               # Video wurde mit diesem Password noch nicht  runtergeladen
             else:
+               
                 return redirect(url_for('prepare_download', session_id=user.sessionId))
         else:
+
             return render_template('fon_code_not_found.html')
+        
     return render_template('fon_index.html')
-    # return render_template('fon_prepare_download.html')
 
 
+
+
+
+############################################################################
+###################2. Bereitstellung  des Video Downloadprozess ############
+############################################################################
 
 @app.route('/prepare_download/<session_id>')
 def prepare_download(session_id):
     return render_template('fon_prepare_download.html', session_id=session_id)
 
 
+
+
+
+############################################################################
+###################3. Versendung  des Videofiles ##########################
+############################################################################
+
 @app.route('/download/<session_id>')
 def download(session_id):
+
     user = User.query.filter_by(sessionId=session_id).first()
 
     if user and not user.downloaded:
         video_file = user.videoFile[7:] if user.videoFile.startswith('static/') else user.videoFile
         try:
             response = send_from_directory(app.config['STATIC_FOLDER'], video_file, as_attachment=True)
-
-            # Delete the video file
-            # video_path = os.path.join(app.config['STATIC_FOLDER'], video_file)
-            # if os.path.exists(video_path):
-            #     os.remove(video_path)
-
             user.downloaded = True
             db.session.commit()
+
             return response
         except Exception as e:
             print(str(e))
             abort(500, description="Error during file download")
     else:
         return render_template('fon_video_downloaded.html')
+    
 
+
+
+############################################################################
+###################3. Danke  und Verabschiedung  ##########################
+############################################################################
 @app.route('/thanks')
 def thanks():
     return render_template('fon_download_success.html')
+
+
+
 
 if (__name__ == '__main__'):
     app.run(debug=True, port=8081, host ='0.0.0.0')

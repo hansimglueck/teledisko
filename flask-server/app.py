e0405ae77b82361d64e737387fe63be4e64c4fd1
8JupiterMoll8 <bautista@gmx.de>
2023-05-19 22:05:43 +0100

    change port to 5000

--- flask-server/app.py
+++ flask-server/app.py
@@ -21,5 +21,5 @@ if (__name__ == '__main__'):
     #     print('Creating database tables...')
     #     db.create_all()
     #     print('Done.')
-    app.run(debug=True, port= 5051, host='0.0.0.0')
+    app.run(debug=True, port= 5000, host='0.0.0.0')
 
